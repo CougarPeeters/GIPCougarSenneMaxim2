fea27e37d6d1584cb038b54533ec574374ec5d58
CougarPeeters <ImHotaru@outlook.com>
2020-03-10 12:00:48 +0100

    Index, toevoegen

---
+++ GIPHoodie/GIPHoodie/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GIPHoodie.Models;
+using GIPHoodie.Persistence;
+using Microsoft.AspNetCore.Http;
+
+namespace GIPHoodie.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersistenceCode persistenceCode = new PersistenceCode();
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetInt32("id", 1);
+            ArtikelRepository artikelRepo = new ArtikelRepository();
+            artikelRepo.Artikels = persistenceCode.loadArtikels();
+            return View(artikelRepo);
+        }
+
+        //[HttpPost]
+
+        //public IActionResult Index()
+        //{
+
+        //}
+
+
+        public IActionResult Toevoegen(int ArtID)
+        {
+            Artikel GeselecteerdeArtikel = persistenceCode.loadArtikel(ArtID);
+            VMArtikelAantal vmArtikelAantal = new VMArtikelAantal();
+            vmArtikelAantal.GeselecteerdArtikel = GeselecteerdeArtikel;
+
+            return View(vmArtikelAantal);
+        }
+    }
+}
---
+++ GIPHoodie/GIPHoodie/Persistence/PersistenceCode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GIPHoodie.Models;
+using MySql.Data.MySqlClient;
+
+namespace GIPHoodie.Persistence
+{
+    public class PersistenceCode
+    {
+        string connStr = "server=localhost;user id=root;password=Test123;database=dbwebshop";
+
+        public List<Artikel> loadArtikels() //laden van de verschillende artikels in de webshop
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string qry = "select * from tblartikel";
+            MySqlCommand cmd = new MySqlCommand(qry, conn);
+            MySqlDataReader dtr = cmd.ExecuteReader();
+            List<Artikel> lijst = new List<Artikel>();
+            while (dtr.Read())
+            {
+                Artikel artikel = new Artikel();
+                artikel.ArtikelID = Convert.ToInt32(dtr["artikelid"]);
+                artikel.Naam = Convert.ToString(dtr["naam"]);
+                artikel.Voorraad = Convert.ToInt32(dtr["voorraad"]);
+                artikel.Prijs = Convert.ToDouble(dtr["prijs"]);
+                artikel.Foto = Convert.ToString(dtr["foto"]);
+                lijst.Add(artikel);
+            }
+            conn.Close();
+            return lijst;
+        }
+
+
+        public Artikel loadArtikel(int ArtID) //laden van het geselecteerde artikel om deze in de winkelmand toe te voegen
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string qry = "select * from tblartikel where artikelid=" + ArtID;
+            MySqlCommand cmd = new MySqlCommand(qry, conn);
+            MySqlDataReader dtr = cmd.ExecuteReader();
+            Artikel artikel = new Artikel();
+            while (dtr.Read())
+            {
+                artikel.ArtikelID = Convert.ToInt32(dtr["artikelid"]);
+                artikel.Naam = Convert.ToString(dtr["naam"]);
+                artikel.Voorraad = Convert.ToInt32(dtr["voorraad"]);
+                artikel.Prijs = Convert.ToDouble(dtr["prijs"]);
+                artikel.Foto = Convert.ToString(dtr["foto"]);
+            }
+            conn.Close();
+            return artikel;
+        }
+
+        public void PasMandAan(WinkelmandItem winkelmanditem) // een geselecteerd artikel in de database in een winkelmand opslaan of aanpassen
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string qry1 = "select * from tblartikel where KlantID=" + winkelmanditem.KlantNr + " and ArtikelID="
+                + winkelmanditem.ArtikelNr;
+            MySqlCommand cmd = new MySqlCommand(qry1, conn);
+            MySqlDataReader dtr = cmd.ExecuteReader();
+            if(dtr.HasRows)
+            {
+                string qry2 = "update tblwinkelmand SET Aantal = '" + winkelmanditem.Aantal + "' where(KlantID = '1') and(ArtikelID = '1')";
+            }
+            else
+            {
+                string qry3 = "insert into tblwinkelmand(KlantID, ArtikelID, Aantal) values(" + winkelmanditem.KlantNr +
+                    "," + winkelmanditem.ArtikelNr + "," + winkelmanditem.Aantal + ")";
+            }
+        }
+    }
+}
